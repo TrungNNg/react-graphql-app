c9aa74de2d031159a0ff747688ea4bf1ef1b26db
TrungNNg <tnguy384@mail.ccsf.edu>
2020-10-25 17:31:25 -0400

    implement local state

--- src/Repository/RepositoryItem/index.js
+++ src/Repository/RepositoryItem/index.js
@@ -3,6 +3,7 @@ import gql from "graphql-tag";
 import Link from "../../Link";
 import Button from "../../Button";
 import { Mutation } from "react-apollo";
+import REPOSITORY_FRAGMENT from '../fragments'
 import "../style.css";
 
 const STAR_REPOSITORY = gql`
@@ -15,6 +16,30 @@ const STAR_REPOSITORY = gql`
     }
   }
 `;
+
+const updateAddStar = (
+  client,
+  { data: { addStar: { starrable: { id } } } },
+) => {
+  const repository = client.readFragment({
+    id: `Repository:${id}`,
+    fragment: REPOSITORY_FRAGMENT,
+  });
+ 
+  const totalCount = repository.stargazers.totalCount + 1;
+ 
+  client.writeFragment({
+    id: `Repository:${id}`,
+    fragment: REPOSITORY_FRAGMENT,
+    data: {
+      ...repository,
+      stargazers: {
+        ...repository.stargazers,
+        totalCount,
+      },
+    },
+  });
+};
 
 const RepositoryItem = ({
   id,
@@ -36,7 +61,7 @@ const RepositoryItem = ({
 
       <div>
         {!viewerHasStarred ? (
-          <Mutation mutation={STAR_REPOSITORY} variables={{ id }}>
+          <Mutation mutation={STAR_REPOSITORY} variables={{ id }} update={updateAddStar}>
             {(addStar, { data, loading, error }) => (
               <Button
                 className={"RepositoryItem-title-action"}
